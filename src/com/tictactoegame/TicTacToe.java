3ab427baba46271bdd5bbc4749ed15592d5183a2
MKS1998 <nksayyad2900@outlook.com>
2021-10-24 18:39:45 +0530

    Added : Checking the index for space before move

--- src/com/tictactoegame/TicTacToe.java
+++ src/com/tictactoegame/TicTacToe.java
@@ -6,13 +6,14 @@ public class TicTacToe {
 
 	static char[] board = new char[10];
 	static Scanner sc = new Scanner(System.in);
+
 	public static void ticTakToeInitialization() {
 		for (int i = 1; i < board.length; i++) {
 			board[i] = ' ';
 		}
 	}
 
-	private static void playerChoose() {	
+	private static void playerChoose() {
 		System.out.println("Player choose X or O");
 		char playerChoose = sc.next().charAt(0);
 		if (playerChoose == 'X' || playerChoose == 'x') {
@@ -32,25 +33,30 @@ public class TicTacToe {
 		System.out.println(board[7] + " | " + board[8] + " | " + board[9]);
 
 	}
-	public static void playerMove() {
-        System.out.println("Select Position in Index to Move in Board (1 to 9) : ");
-        int indexNumber = sc.nextInt();
-        if (indexNumber < 1 || indexNumber > 9) {
-            System.out.println("You Entered Invalid Position ");
-            playerMove();
-        } else {
-            char playerChoice = 0;
-			board[indexNumber] = playerChoice;
-            showBoard();
-        }
 
-    }
+	public static void playerMove() {
+		System.out.println("Select Position in Index to Move in Board (1 to 9) : ");
+		int indexNumber = sc.nextInt();
+		if (indexNumber < 1 || indexNumber > 9) {
+			System.out.println("You Entered Invalid Position ");
+			playerMove();
+		} else if (board[indexNumber] != ' ') {
+			System.out.println("The index already occupied kindly choose another index " + "by observing game Board ");
+			playerMove();
+		} else {
+			char playerChoice = 0;
+			board[indexNumber] = playerChoice ;
+			showBoard();
+		}
+
+	}
 
 	public static void main(String[] args) {
 		System.out.println("Welcome to TicTacToe Game");
 		ticTakToeInitialization();
 		playerChoose();
 		showBoard();
+		playerMove();
 	}
 
 }
